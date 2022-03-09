4a48cd1522f734ce682ef4e611a4a19237cf99e1
Your Name <you@example.com>
2022-03-09 22:56:27 +0000

    add nasm folder

--- gas/add/add.s
+++ gas/add/add.s
@@ -6,14 +6,14 @@ data:
 .global _start
  
 _start:
-    nop     
-    movl    $0,%eax
-    movl    $0,%ebx
-    movl    $0,%ecx
-    movb    $20,%al
-    addb    $10,% al
-    addb    $30,% al
-    movl    $124,%ebx
-    addl    data,%ebx
-    nop
-    nop
+	nop     
+	movl    	$0,%eax
+	movl    	$0,%ebx
+	movl    	$0,%ecx
+	movl    	$20,%al
+	addb    	$10,% al
+ 	addb		$30,% al
+ 	movl    	$124,%ebx
+    	addb    	data,%ebx
+    	nop
+    	nop
